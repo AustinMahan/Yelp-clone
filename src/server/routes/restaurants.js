7c1f43fc905e71564f31a01bfdf7e43058b08638
AustinMahan <amahan0424@gmail.com>
2016-09-14 12:47:54 -0600

    new rest form

--- src/server/routes/restaurants.js
+++ src/server/routes/restaurants.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res, next) {
 
 router.get('/new', function (req, res, next) {
   const renderObj = {};
-  res.render('newRest')
+  res.render('newRest', {});
 });
 
 router.get('/:id', function (req, res, next) {
@@ -38,7 +38,12 @@ router.get('/:id/reviews/new', function (req, res, next) {
 });
 
 router.post('/new', function (req, res, next) {
-
-})
+  res.send(req.body)
+  var type = req.body.type
+  var name = req.body.name
+  var streetAddress = req.body.streetAddress
+  var city = req.body.city
+  var location = `${req.body.streetAddress}, ${req.body.city}, ${req.body.state}`
+});
 
 module.exports = router;
