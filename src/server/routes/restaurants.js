4135701c1d7b3ba07080cd0064e9b156211d091d
AustinMahan <amahan0424@gmail.com>
2016-09-14 11:11:57 -0600

    more routes

--- src/server/routes/restaurants.js
+++ src/server/routes/restaurants.js
@@ -12,5 +12,25 @@ router.get('/:id', function (req, res, next) {
 
 });
 
+router.get('/:id/reviews', function (req, res, next) {
+  const renderObj = {};
+
+});
+
+router.get('/:id/edit', function (req, res, next) {
+  const renderObj = {};
+
+});
+
+router.get('/:id/review/:revId/edit', function (req, res, next) {
+  const renderObj = {};
+
+});
+
+router.get('/:id/reviews/new', function (req, res, next) {
+  const renderObj = {};
+
+});
+
 
 module.exports = router;
