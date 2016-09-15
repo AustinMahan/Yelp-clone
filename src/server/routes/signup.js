f19897842c53fd3843e03f0c481b66ba9670f0fc
AustinMahan <amahan0424@gmail.com>
2016-09-15 10:00:54 -0600

    semicolons

--- src/server/routes/signup.js
+++ src/server/routes/signup.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 router.get('/', function (req, res, next) {
   const renderObj = {};
-  res.render('signupForm')
+  res.render('signupForm');
 });
 
 router.get('/err', function (req, res, next) {
   const renderObj = {};
   renderObj.err = true;
-  res.render('signupForm', renderObj)
-})
+  res.render('signupForm', renderObj);
+});
 
 router.post('/', function (req, res, next) {
-  var password = bcrypt.hashSync(req.body.password, 10)
+  var password = bcrypt.hashSync(req.body.password, 10);
   knex('users').where('username', req.body.username).then((data) => {
-    if(data.length > 0) {
-      res.redirect('/signup/err')
+    if (data.length > 0) {
+      res.redirect('/signup/err');
     }else {
       knex('users').insert({first_name: req.body.first_name, last_name: req.body.last_name, username: req.body.username, password: password }).then(() => {
-        res.redirect('restaurants')
-      })
+        res.redirect('restaurants');
+      });
     }
-  })
+  });
 });
 
 module.exports = router;
