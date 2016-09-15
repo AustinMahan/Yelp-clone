750748b48ece20e1dd166858f3bb1c8583cf2d65
AustinMahan <amahan0424@gmail.com>
2016-09-15 13:22:35 -0600

    seeding pic url

--- src/server/db/seeds/02restraunts.js
+++ src/server/db/seeds/02restraunts.js
@@ -1,5 +1,9 @@
 var faker = require('faker');
 var types = ['Mexican', 'American', 'Italian', 'Chinese'];
+var i = 0
+function changeI() {
+  i = Math.floor(Math.random() * 4)
+}
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
@@ -8,18 +12,31 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('restaurants').insert({name: 'qDoba', type: 'Mexican', description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 1))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 2))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 3))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 4))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 5))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 6))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 7))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 8))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 9))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 10))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 11))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 12))}),
-        knex('restaurants').insert({name: faker.company.companyName(), type: types[Math.floor(Math.random() * 4)], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 13))})
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i],
+        description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 2))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 3))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 4))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 5))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 6))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 7))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 8))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 9))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 10))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 11))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 12))}),
+        changeI(),
+        knex('restaurants').insert({name: faker.company.companyName(), type: types[i], description: faker.lorem.paragraph(), location: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`, url: faker.image.food(), avg_review: (knex('reviews').avg('rating').where('restaurant_id', 13))})
       ]);
     });
 };
