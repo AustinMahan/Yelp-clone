1a02e53b42426626bce81a1e8d94dbbb3c5e5cd1
AustinMahan <amahan0424@gmail.com>
2016-09-15 16:14:37 -0600

    seeds

--- src/server/db/seeds/05restrauntRatings.js
+++ src/server/db/seeds/05restrauntRatings.js
@@ -5,7 +5,6 @@ var types = ['Mexican', 'American', 'Italian', 'Chinese'];
 exports.seed = function(knex, Promise) {
 
   return knex('restaurants').select('id').orderBy('id').then((data) => {
-    console.log(data);
     var promise = data.map(function (curId) {
       return knex('restaurants').where('id', curId.id).update('avg_review', knex('reviews').avg('rating').where('restaurant_id', curId.id))
     })
